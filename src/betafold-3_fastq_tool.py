98d1c8a7083c8fcbfbec7d2282ff76ba8cc7e100
iliapopov17 <iljapopov17@gmail.com>
2023-10-08 10:44:21 +0300

    Add length_check() function to betafold-3_fastq_tool.py file
    This function checks if the length of a DNA sequence in fastq file lies within specified bounds
    
    Parameters:
    - seq (str): DNA sequence
    - length_bounds (tuple): tuple with bottom and top bounds for sequence length
    
    Returns:
    - bool: True if the length is within bounds, False if not

--- src/betafold-3_fastq_tool.py
+++ src/betafold-3_fastq_tool.py
@@ -21,3 +21,20 @@ def gc_check(sequence: str, gc_bounds: tuple) -> bool:
     return bottom_threshold <= gc <= top_threshold
 
 
+def length_check(sequence: str, length_bounds: tuple) -> bool:
+    """
+    This function checks if the length of a DNA sequence in fastq file lies within specified bounds
+
+    Parameters:
+    - seq (str): DNA sequence
+    - length_bounds (tuple): tuple with bottom and top bounds for sequence length
+
+    Returns:
+    - bool: True if the length is within bounds, False if not
+    """
+    
+    bottom_threshold, top_threshold = length_bounds
+
+    return bottom_threshold <= len(sequence) <= top_threshold
+
+
