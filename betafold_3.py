994bdbfaab0e3d6fd6279c98a5a073660c8e4329
iliapopov17 <iljapopov17@gmail.com>
2023-10-19 14:02:36 +0300

    Major fix to main script

--- betafold_3.py
+++ betafold_3.py
@@ -1,3 +1,7 @@
+import os
+from typing import Union, Tuple
+
+
 from src import betafold_3_amino_analyzer
 from src import betafold_3_dna_rna_tools
 from src import betafold_3_fastq_tool
@@ -28,19 +32,19 @@ def run_amino_analyzer(sequence: str, procedure: str, *, weight_type: str = 'ave
     if procedure not in procedures:
         raise ValueError(f'Incorrect procedure. Acceptable procedures: {", ".join(procedures)}')
 
-    if not is_aa(sequence):
+    if not betafold_3_amino_analyzer.is_aa(sequence):
         raise ValueError('Incorrect sequence. Only amino acids are allowed (V, I, L, E, Q, D, N, H, W, F, Y, R, K, S, T, M, A, G, P, C, v, i, l, e, q, d, n, h, w, f, y, r, k, s, t, m, a, g, p, c).')
 
     if procedure == 'aa_weight':
-        result = aa_weight(sequence, weight_type)
+        result = betafold_3_amino_analyzer.aa_weight(sequence, weight_type)
     elif procedure == 'count_hydroaffinity':
-        result = count_hydroaffinity(sequence)
+        result = betafold_3_amino_analyzer.count_hydroaffinity(sequence)
     elif procedure == 'peptide_cutter':
-        result = peptide_cutter(sequence, enzyme)
+        result = betafold_3_amino_analyzer.peptide_cutter(sequence, enzyme)
     elif procedure == 'one_to_three_letter_code':
-        result = one_to_three_letter_code(sequence)
+        result = betafold_3_amino_analyzer.one_to_three_letter_code(sequence)
     elif procedure == 'sulphur_containing_aa_counter':
-        result = sulphur_containing_aa_counter(sequence)
+        result = betafold_3_amino_analyzer.sulphur_containing_aa_counter(sequence)
     return result
 
 
@@ -76,13 +80,13 @@ def run_dna_rna_tools(*args: str) -> str:
 
     for sequence in sequences:
         if procedure == 'transcribe':
-            result = transcribe(sequence)
+            result = betafold_3_dna_rna_tools.transcribe(sequence)
         elif procedure == 'reverse':
-            result = reverse(sequence)
+            result = betafold_3_dna_rna_tools.reverse(sequence)
         elif procedure == 'complement':
-            result = complement(sequence)
+            result = betafold_3_dna_rna_tools.complement(sequence)
         elif procedure == 'reverse_complement':
-            result = reverse_complement(sequence)
+            result = betafold_3_dna_rna_tools.reverse_complement(sequence)
         results.append(result)
 
     if len(results) == 1:
@@ -92,63 +96,42 @@ def run_dna_rna_tools(*args: str) -> str:
     
 
 def run_fastq_tool(
-    seqs: dict,
-    gc_bounds: tuple = (0, 100),
-    length_bounds: tuple = (0, 2**32),
+    input_path: str,
+    gc_bounds: Union[int, float, Tuple[Union[int, float], Union[int, float]]]= (0, 100),
+    length_bounds: Union[int, Tuple[int, int]] = (0, 2**32),
     quality_threshold: int = 0,
-) -> dict:
+    output_filename: str = None) -> None:
     '''
     This is the main function that filters the DNA sequences in a fastq file based on specified criteria
 
     Parameters:
-    - seqs (dict): dictionary where keys are sequence identifiers and values are tuples containing sequence and quality scores
-    - gc_bounds (tuple): tuple with bottom and top bounds for GC content
-    - length_bounds (tuple): tuple with bottom and top bounds for sequence length
+    - input_path (str): path to fastq file
+    - gc_bounds (Union[int, float, Tuple[Union[int,float], Union[int,float]]): bottom and top bounds for GC content
+    - length_bounds (Union[int, Tuple[int, int]]): bottom and top bounds for sequence length
     - quality_threshold (int): acceptable quality
+    - output_filename (str): name of filtered fastq file
 
-    Returns:
-    - dict: filtered dictionary that contains only the sequences that fully match the specified criteria
+    Saves the filtered sequences to a fastq file named input_path/output_filename
     '''
+    seqs = betafold_3_fastq_tool.read_fastq(input_path)
 
-    # ------------------------------------------------------
-    # Check if every parameter is setted up correctly
-    if type(gc_bounds) == int:
+    if isinstance(gc_bounds, int) or isinstance(gc_bounds, float):
         gc_bounds = (0, gc_bounds)
-
-    elif len(gc_bounds) == 1:
-        gc_bounds = (0, gc_bounds[0])
-
-    elif len(gc_bounds) > 2:
-        raise ValueError('Invalid gc_bound value!')
-
-    elif type(gc_bounds) != tuple:
-        raise ValueError('Input tuple value!')
-
-    if type(length_bounds) == int:
+    if isinstance(length_bounds, int) or isinstance(length_bounds, float):
         length_bounds = (0, length_bounds)
-
-    elif len(length_bounds) == 1:
-        length_bounds = (0, length_bounds[0])
-
-    elif len(length_bounds) > 2:
-        raise ValueError('Invalid length_bounds value!')
-
-    elif type(length_bounds) != tuple:
-        raise ValueError('Input tuple value!')
-
-    if len(str(quality_threshold)) > 2:
-        raise ValueError('Invalid quality_threshold value!')
-    
-    elif type(quality_threshold) != int:
-        raise ValueError('Input integer value!')
-    # ------------------------------------------------------
 
     filtered_seqs = {}
     
     for name, (sequence, quality) in seqs.items():        
-        if (gc_check(sequence, gc_bounds) 
-            and length_check(sequence, length_bounds) 
-            and quality_check(quality, quality_threshold)):
+        if (betafold_3_fastq_tool.gc_check(sequence, gc_bounds) 
+            and betafold_3_fastq_tool.length_check(sequence, length_bounds) 
+            and betafold_3_fastq_tool.quality_check(quality, quality_threshold)):
             filtered_seqs[name] = (sequence, quality)
     
-    return filtered_seqs
+    if output_filename is None:
+        output_filename = os.path.basename(input_path)
+        
+    else:
+        output_filename += '.fastq'
+        
+    betafold_3_fastq_tool.write_fastq(output_filename, filtered_seqs)
